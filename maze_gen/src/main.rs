e98b654f8c1f78110be45c5b2eca9646a1e4410f
Daniel Campbell <campbelldj@hotmail.com>
2015-11-17 11:03:13 +1300

    Small refactor + implemented and tested display

--- maze_gen/src/main.rs
+++ maze_gen/src/main.rs
@@ -1,16 +1,29 @@
+use std::fmt;
+
 enum Cell {
     Wall,
     Empty,
 }
 
-struct BackTracker {
-    width:  usize,
+pub struct Maze {
+    cells: Vec<Cell>,
+    width: usize,
     height: usize,
+}
+
+pub struct Backtracker {
     seed:   i32,
 }
 
+impl Maze {
+
+    pub fn new(width: usize, height: usize) -> Maze {
+        Maze {cells: vec![Cell::Wall; width*height], width: width, height: height}
+    }
+}
+
 trait MazeGen {
-    fn gen(&self) -> Vec<Cell>;
+    fn gen(&self, width: usize, height: usize) -> Maze;
 }
 
 impl Clone for Cell {
@@ -22,14 +35,42 @@ impl Clone for Cell {
     }
 }
 
-impl MazeGen for BackTracker {
-    fn gen(&self) -> Vec<Cell> {
-        let size = self.width*self.height;
-        let mut result = vec![Cell::Wall; size];
-        result
+impl MazeGen for Backtracker {
+    fn gen(&self, width: usize, height: usize) -> Maze {
+        let size = width*height;
+        let mut result = Maze::new(width, height);
+        unimplemented!()
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+impl fmt::Display for Maze {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut result = "".to_owned();
+        for i in 0..self.height {
+            for j in 0..self.width {
+                result.push_str(match self.cells[i*self.width + j] {
+                    Cell::Wall  => "X",
+                    Cell::Empty => " "
+                });
+            }
+            result.push_str("\n");
+        }
+        write!(f, "{}", result)
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display_empty() {
+        let maze = Maze::new(1, 1);
+        assert_eq!(format!("{}", maze), "X\n");
+        let maze = Maze::new(2, 2);
+        assert_eq!(format!("{}", maze), "XX\nXX\n");
+        let maze = Maze::new(1, 2);
+        assert_eq!(format!("{}", maze), "X\nX\n");
+        let maze = Maze::new(2, 1);
+        assert_eq!(format!("{}", maze), "XX\n");
+    }
 }
