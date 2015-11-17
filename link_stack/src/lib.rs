9643c6fd1f53df994e1f0b73463e63476c927401
Daniel Campbell <campbelldj@hotmail.com>
2015-11-17 17:05:10 +1300

    Added FizzBuzz macro

--- link_stack/src/lib.rs
+++ link_stack/src/lib.rs
@@ -187,6 +187,31 @@ macro_rules! stack_contains_all {
     }};
 }
 
+//Designed to emulate fizz-buzz, or any variant thereof (Returns a string)
+//First expr must be some kind of integer iterator (eg. a range)
+//Then a sequence of tuples of the form (number, string).
+//For every number in the range, will append a line: either all strings
+//for the numbers it is a multiple of, or just the number itself
+macro_rules! fizz_buzz {
+    ($e:expr, $(($n:expr, $s:expr)),*) => {{
+        let mut output = "".to_owned();
+        for i in $e {
+            let mut tmp = "".to_owned();
+            $(
+                if i % $n == 0 {
+                    tmp.push_str($s);
+                } 
+            )*
+            if tmp.len() == 0 {
+                tmp = i.to_string();
+            }
+            tmp.push_str("\n");
+            output.push_str(&tmp);
+        }
+        output
+    }};
+}
+
 
 #[cfg(test)]
 mod test {
@@ -311,5 +336,11 @@ mod test {
         assert!(!stack_contains!(stack, 4, 5));
         assert!(stack_contains_all!(stack, 1, 2, 3));
         assert!(!stack_contains_all!(stack, 1, 2, 3, 4));
+    }
+
+    #[test]
+    fn macro_fizzbuzz() {
+        let fizzbuzz = fizz_buzz!(1..16, (3, "Fizz"), (5, "Buzz"));
+        assert_eq!(fizzbuzz, "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n");
     }
 }
